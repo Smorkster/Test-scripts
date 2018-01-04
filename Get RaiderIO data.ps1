280d6a0053d766ec8bad31288a17ab51785dbb48
Smorkster <smorkster@gmail.com>
2018-01-04 22:57:09 +0100

    New script "Get RaiderIO data"

---
+++ Get RaiderIO data.ps1
@@ -0,0 +1,5 @@
+﻿$char = Read-Host "Charactername"
+$realm = Read-Host "Realm"
+$answer = Invoke-WebRequest -Uri "https://raider.io/api/v1/characters/profile?region=eu&realm=$realm&name=$char&fields=mythic_plus_scores%2Cmythic_plus_highest_level_runs" | ConvertFrom-Json
+$answer.mythic_plus_highest_level_runs[0].mythic_level
+$answer.mythic_plus_scores.all
